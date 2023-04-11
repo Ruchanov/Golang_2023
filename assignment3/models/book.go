1441def38f92b8c9d3087e9acf3f8182f19e6b6d
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2023-04-11 15:59:01 +0600

    upd

--- assignment3/models/book.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Book struct {
-	Id          uint   `json:"id" gorm: "primary key"`
-	Title       string `json: "title"`
-	Description string `json: "description"`
-	Cost        int    `json: "cost"`
-}
